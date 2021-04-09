92661b58f8fc580b348df2a0ed2475c61af4963f
Elir25 <elirendiles@gmail.com>
2021-04-09 12:25:56 +0200

    As6 ex 1: finished 1, 2 and 3

--- InClass/A-ES6/exercise1.js
+++ InClass/A-ES6/exercise1.js
@@ -1,18 +1,27 @@
 // Turn this function into an arrow function
-function arrow() {
+
+/*function arrow() {
   return "es6 is awesome!";
-}
+}*/
+const arrow = () => "es6 is awesome";
+console.log(arrow())
 
 //Use the es6 syntax for default parameter
-function defaultParameter(name) {
-  var name = name || "sam";
+function defaultParameter(name = "sam") {
+  //var name = name || "sam";
   return name;
 }
+console.log(defaultParameter())
 
+arr1 = ["hello ", "you"]
+arr2 = ["I'm ", "Eli"]
 // Use the spread operator to combine arr1 and arr2
 function combineArrays(arr1, arr2) {
-  return arr1.concat(arr2);
+  //return arr1.concat(arr2);
+  const combineArr1And2 = [...arr1, ...arr2];
+  return combineArr1And2;
 }
+console.log(combineArrays(arr1, arr2))
 
 //use destructuring to return the object's cyf property
 function destructuring(obj) {
